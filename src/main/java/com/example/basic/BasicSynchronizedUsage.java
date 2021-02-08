eb08105c1eafb9c4ee547ae32baed3b65da4729f
elisa.natalie <elisa.natalie@smaato.com>
2021-02-09 00:10:00 +0100

    Synchronized

---
+++ src/main/java/com/example/basic/BasicSynchronizedUsage.java
@@ -0,0 +1,32 @@
+package com.example.basic;
+
+import java.util.stream.IntStream;
+
+public class BasicSynchronizedUsage {
+    private int count;
+
+    public static void main(String[] args) {
+        BasicSynchronizedUsage app = new BasicSynchronizedUsage();
+        app.doTask();
+    }
+
+    public void doTask() {
+        Thread thread1 = new Thread(() -> IntStream.range(0, 10000).forEach(i -> increment()));
+        Thread thread2 = new Thread(() -> IntStream.range(0, 10000).forEach(i -> increment()));
+
+        thread1.start();
+        thread2.start();
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Count is :" + count);
+    }
+
+    public synchronized void increment() {
+        count++;
+    }
+}
